20c914c00d379e187e7d3025cb9b66098fff6ef1
Gilbert563307 <ssempijja759@gmail.com>
2024-01-16 01:07:01 +0100

    added the create todo function

--- customhooks/useCustomCollectTodosHook.jsx
+++ customhooks/useCustomCollectTodosHook.jsx
@@ -8,20 +8,26 @@ export default function useCustomCollectTodosHook() {
 
   useEffect(() => {
     const retrieveTodos = async () => {
-      if (state instanceof Promise) {
-        state.then((handledState) => {
+      try {
+        if (state instanceof Promise) {
+          const handledState = await state;
           if (handledState.todos) {
             setTodos(handledState.todos);
-            console.log(`state is changing`, handledState.todos);
+            console.log("State is changing", handledState.todos);
           }
 
           if (handledState.message) {
             setMessage(handledState.message);
           }
-        });
+        }
+      } catch (error) {
+        console.error("Error retrieving todos:", error);
+        setMessage(`Error retrieving todos, ${error.message}}`);
       }
     };
+
     retrieveTodos();
   }, [state]);
+
   return { todos, message };
 }
